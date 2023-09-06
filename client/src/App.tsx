341816177c35d306c380d971dac505dd65733721
sacha0914 <sacha0914.poidevin@gmail.com>
2023-09-06 22:19:22 +0200

    docker prod

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react';
+import  { useState, ChangeEvent, FormEvent, useEffect } from 'react';
 import axios from 'axios';
 import './App.css'; // Importez le fichier CSS
 
